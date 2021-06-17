b84cd0c56ec920e67dd68f2fda7cfc743a5ca2b3
Raghav Kapoor <raghav.kapoor@affordplan.com>
2021-06-17 23:19:08 +0530

    added some code

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Row from './Row';
+
+function App() {
+  return (
+    <div className="App">
+      <Row title ="Netflix"/>
+      <Row title ="Trending Now"/>
+    </div>
+  );
+}
+
+export default App;
